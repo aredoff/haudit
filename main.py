8f5a393c3d7e41d42f963d7faef50e5c83ecc4bc
Александр Краснов <aleksandr.krasnov@hoster.by>
2021-07-23 19:53:32 +0300

    Remove custom data to env

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()
 ssh_user = os.getenv('SSH_USER')
 ssh_password = os.getenv('SSH_PASSWORD')
+test_hostname = os.getenv('TEST_HOSTNAME')
 
-a = Audit(hostname='officeradio.hosterby.com', username=ssh_user, password=ssh_password)
+a = Audit(hostname=test_hostname, username=ssh_user, password=ssh_password)
 report = a.execute_queries()
 a.printReport(report)
